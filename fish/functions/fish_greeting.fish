e3fac92996cf9825c9189bfcfd084fbfb6bc2f45
MD. Annfee Islam <islamannafee2313@gmail.com>
2026-05-25 13:50:38 +0600

    Update fish greeting ASCII art

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,10 +1,10 @@
 function fish_greeting
     echo -ne '\x1b[38;5;16m'  # Set colour to primary
-    echo '    _   __           __                         '
-    echo '   / | / /___  _____/ /___  __________   ___    '
-    echo '  /  |/ / __ \/ ___/ __/ / / / ___/ __ \/ _ \    '
-    echo ' / /|  / /_/ / /__/ /_/ /_/ / /  / / / /  __/    '
-    echo '/_/ |_/\____/\___/\__/\__,_/_/  /_/ /_/\___/     '
+    echo ' _    __              ___         '
+    echo '| |  / /___ _ ___    / (_) ____   '
+    echo '| | / / __ `// _ \  / / / / ___/  '
+    echo '| |/ / /_/ //  __/ / / / (__  )   '
+    echo '|___/\__,_/ \___/ /_/_/ /____/    '
     set_color normal
     fastfetch --key-padding-left 5
 end
